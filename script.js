63ee3f3eed639f66f90303748a3a48c4dc6d430e
Jonas Blu <jblu3541@gmail.com>
2026-09-07 06:30:51 +0700

    script.js

--- script.js
+++ script.js
@@ -200,53 +200,12 @@ let win=Math.floor(Math.random()*hadiah.length);
 
 });
 
-spinBtn.onclick = () => {
+spinBtn.disabled=false;
 
-    const popup = document.createElement("div");
-
-    popup.innerHTML = `
-        <div style="
-            position:fixed;
-            inset:0;
-            background:rgba(0,0,0,.85);
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            z-index:999999;
-            padding:20px;
-        ">
-            <div style="position:relative;max-width:600px;width:100%;">
-                
-                <button onclick="this.parentElement.parentElement.parentElement.remove()" style="
-                    position:absolute;
-                    right:-10px;
-                    top:-10px;
-                    width:35px;
-                    height:35px;
-                    border:0;
-                    border-radius:50%;
-                    background:#ffcc00;
-                    color:#000;
-                    font-size:24px;
-                    font-weight:bold;
-                    cursor:pointer;
-                    z-index:2;
-                ">×</button>
-
-                <img src="URL-FOTO-KAMU" style="
-                    display:block;
-                    width:100%;
-                    max-height:85vh;
-                    object-fit:contain;
-                    border-radius:12px;
-                ">
-
-            </div>
-        </div>
-    `;
-
-    document.body.appendChild(popup);
+spinBtn.onclick=()=>{
+    window.location.href="https://ragam4d03.com/";
 };
+        },6000);
 
     })
  .catch(err=>{
